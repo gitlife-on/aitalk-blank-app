00e3f5e18538ca41765e875e69358fe975ebf66c
jaytn <gitlife@gmail.com>
2025-01-09 12:43:39 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("AI토크-스트림릿 실습 앱")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
